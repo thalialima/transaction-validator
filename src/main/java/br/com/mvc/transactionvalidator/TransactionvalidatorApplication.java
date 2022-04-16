0229474fd5be63bdcd96a59a7dd2564ee9bf91fd
Thalia Lima <thaliairene20@gmail.com>
2022-04-16 11:55:44 -0300

    first commit

---
+++ src/main/java/br/com/mvc/transactionvalidator/TransactionvalidatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.mvc.transactionvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionvalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionvalidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mvc/transactionvalidator/TransactionvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mvc.transactionvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionvalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
